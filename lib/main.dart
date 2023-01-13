35c9c28b6973bd28b63d97f433822e1c5e96a538
sametates4 <sametates4@outlook.com>
2023-01-13 15:46:42 +0300

    material 3 false

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.lightBlue,
-          useMaterial3: true,
           scaffoldBackgroundColor: const Color.fromRGBO(250, 228, 218, 1),
           appBarTheme: const AppBarTheme(
             backgroundColor: Color.fromRGBO(250, 228, 218, 1),
